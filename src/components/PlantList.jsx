2471af0ba73bd222d78a857a2da7a3f060ceba11
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-05-15 10:25:00 +0200

    complete all test

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import PlantCard from "./PlantCard";
+// src/components/PlantList.jsx
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+import React from "react";
+import PlantCard from "./PlantCard"; // Make sure this path and filename are exactly correct
+
+function PlantList({ plants }) {
+  const plantCards = plants.map((plant) => (
+    <PlantCard
+      key={plant.id}
+      plant={plant}
+    />
+  ));
+
+  return <ul className="cards">{plantCards}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Fetch plants when component loads
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // Add new plant to state
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]); // You can also use setPlants(prev => [...prev, newPlant])
+  }
+
+  // Filter plants by search term
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
